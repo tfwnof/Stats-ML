c0f969eb568b95ce861b977c269fae3ead47d4f7
Med <93465561+tfwnof@users.noreply.github.com>
2022-07-14 21:14:29 +0400

    Update FecthData.py

--- FecthData.py
+++ FecthData.py
@@ -1,12 +1,14 @@
 import pandas as pd
 import numpy as np
-import datetime
+import datetime as dt
 import investpy
 import wbgapi
 
-#downloading datasets to test on
+#default end dates will be today
+today = dt.datetime.today().strftime('%d/%m/%Y')
 
-today = datetime.datetime.today().strftime('%d/%m/%Y')
-def stockdata(ticker1='AAPL', ticker2='MSFT', startdate='01/01/2020', enddate=today):
-    df = investpy.stocks.get_stock_historical_data(ticker1, country='United States', from_date=startdate, to_date=enddate)
+#get stock data
+def stockdata(ticker='AAPL', listed='United States', startdate='01/01/2020', enddate=today):
+    df = investpy.stocks.get_stock_historical_data(ticker, country=listed, from_date=startdate, to_date=enddate)
     return df
+
