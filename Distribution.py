b6ff296364d2732f6caf4ae0d8a471d4d0ff7c20
Med <93465561+tfwnof@users.noreply.github.com>
2022-07-15 14:12:40 +0100

    Update Distribution.py

--- Distribution.py
+++ Distribution.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from FetchData import stockdata
 
-
+#plots distribution of an array
 def normaldist(array):
     array=np.sort(array)
     
@@ -14,4 +14,17 @@ def normaldist(array):
         return (1/(sd*np.sqrt(2*np.pi)))*np.e**((-1/2)*((x-mean)/sd)**2)
     
     f = [transform(n, np.mean(array), np.std(array)) for n in array]
+    return f, plt.plot(f)
+
+
+#computes returns and plot distribution
+def returnsnormaldist(array):
+    rets = pd.DataFrame(array)
+    rets['Returns'] = rets.iloc[:, 0].pct_change().dropna()
+    rets = rets.sort_values(by=['Returns'], ascending=True)
+    
+    def transform(x, mean=None, sd=None):
+        return (1/(sd*np.sqrt(2*np.pi)))*np.e**((-1/2)*((x-mean)/sd)**2)
+    
+    f = [transform(n, np.mean(rets['Returns']), np.std(rets['Returns'])) for n in rets['Returns']]
     return f, plt.plot(f)
