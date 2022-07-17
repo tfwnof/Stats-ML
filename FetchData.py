57bcfe7b2139091fdfb039db47e7ce7ef6bac881
Med <93465561+tfwnof@users.noreply.github.com>
2022-07-17 11:19:14 +0400

    Update FetchData.py

--- FetchData.py
+++ FetchData.py
@@ -1,18 +1,22 @@
-import pandas as pd
-import numpy as np
-import datetime as dt
+from datetime import datetime, timedelta
 import investpy
 import wbgapi
 
-#default end dates will be today
-today = dt.datetime.today().strftime('%d/%m/%Y')
+#default dates
+today = datetime.date(datetime.now()).strftime('%d/%m/%Y')
+trailingYear = datetime.date(datetime.now()) - timedelta(days=365)
+trailingYear = trailingYear.strftime('%d/%m/%Y')
 
 #get stock data - Loading by default Apple's historical data
-def stockdata(ticker='AAPL', listed='United States', startdate='01/01/2020', enddate=today):
+def stockdata(ticker='AAPL', listed='United States', startdate=trailingYear, enddate=today):
     df = investpy.stocks.get_stock_historical_data(ticker, country=listed, from_date=startdate, to_date=enddate)
     return df
 
+#get indexes
+def stocksindex():
+    df = investpy.indices.get_index_historical_data('NQ US 300', country='United States', from_date=trailingYear, to_date=today)
+    return df
 
 #get GDP
-def GDP():
+def Econdata():
     return
